518fde7c72bb132e6d53ef7f1ecd002e6eb49fe6
Jinxin-neuro <jchen9@uab.edu>
2024-10-09 14:20:43 -0500

    Update AngleLinear.m

--- THIRDEYE/Code/AngleLinear.m
+++ THIRDEYE/Code/AngleLinear.m
@@ -8,23 +8,31 @@
 
 % in the later readings, make sure the label is correct. 
 %%
-function Events = Angleliner(BYBEvents,Angles)
-labeltime = importdata(BYBEvents);
-Label = zeros(1,length(labeltime));
+function Events = AngleLiner(BYBEvents,Angles)
+labeltime = readtable(BYBEvents);
+Label = zeros(1,height(labeltime));
 
-time = zeros(1,length(labeltime));
-for i=1:length(labeltime)
-reading = labeltime{i};
-reading= sscanf(join(reading,''),'[%f,%f]',[2,inf]).';
-Label(i)=reading(1);
-time(i)=reading(2);
+time = zeros(1,height(labeltime));
+for i=1:height(labeltime)
+    % keyboard
+reading = labeltime(i,:);
+reading = table2array(reading);
+% reading= sscanf(join(reading,''),'[%f,%f]',[2,inf]).';
+Label(i)=reading(1,1);
+time(i)=reading(1,2);
 end
+if width(labeltime)>2
+    Events = table2array(labeltime);
+    
+else
 lengthevent = length(Label);
 lengthAngle = length(Angles);
-keyboard
 if lengthevent < lengthAngle
 Events = [Label;time;Angles(1:length(Label))]';
+disp('Hmmm We get less marker than we should')
 else
 Events = [Label(1:length(Angles));time(1:length(Angles));Angles]';
+disp('That looks about right')
+end
 end
 end
